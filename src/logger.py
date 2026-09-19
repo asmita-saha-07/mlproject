234ec3080b47ee00153e5ec94dad9aaf2a6cf33c
sahaasmita60-ship-it <sahaasmita60@gmail.com>
2026-09-20 01:37:57 +0530

    EDA

---
+++ src/logger.py
@@ -0,0 +1,17 @@
+import logging
+import os 
+from datetime import datetime
+from logging.handlers import RotatingFileHandler
+
+logs_path=os.path.join(os.getcwd(),"logs")
+os.makedirs(logs_path, exist_ok=True)
+log_file=os.path.join(logs_path,"app.log")
+
+handler=RotatingFileHandler(log_file, maxBytes=3000, backupCount=3)
+formatter=logging.Formatter("%(asctime)s - %(levelname)s - %(lineno)d - %(message)s")
+handler.setFormatter(formatter)
+
+logger=logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
+
